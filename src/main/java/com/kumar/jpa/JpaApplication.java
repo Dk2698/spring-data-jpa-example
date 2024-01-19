198b2bff6b0ef6df836dac69f6e844b7fb05eac3
kumar1d <dileep.kumar@logistiex.com>
2024-01-19 23:00:06 +0530

    spring data jpa query

--- src/main/java/com/kumar/jpa/JpaApplication.java
+++ src/main/java/com/kumar/jpa/JpaApplication.java
@@ -1,5 +1,6 @@
 package com.kumar.jpa;
 
+import com.github.javafaker.Faker;
 import com.kumar.jpa.models.Author;
 import com.kumar.jpa.models.Video;
 import com.kumar.jpa.repositories.AuthorRepository;
@@ -29,11 +30,22 @@ public class JpaApplication {
 //					.email("test@nomail.com")
 //					.build();
 //			repository.save(author);
-			var video = Video.builder()
-					.name("abc")
-					.length(5)
-					.build();
-			videoRepository.save(video);
+//			var video = Video.builder()
+//					.name("abc")
+//					.length(5)
+//					.build();
+//			videoRepository.save(video);
+
+			for (int i = 0; i < 50; i++) {
+				Faker faker = new Faker();
+				var author = Author.builder()
+						.firstName(faker.name().firstName())
+						.lastName(faker.name().lastName())
+						.age(faker.number().numberBetween(19,50))
+						.email("test"+ i+ "@nomail.com")
+						.build();
+				repository.save(author);
+			}
 		};
 	}
 }
--- src/main/java/com/kumar/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/kumar/jpa/repositories/AuthorRepository.java
@@ -3,5 +3,32 @@ package com.kumar.jpa.repositories;
 import com.kumar.jpa.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    // select * from author where first_name = 'kumar'
+    List<Author> findAllByFirstName(String fn);
+
+    // select * from author where first_name = 'kumar' & last_name= 'r'
+//    List<User> findByFirstNameAndLastName(String fn, String ln);
+
+    // select * from author where first_name = 'kumar'
+    List<Author> findAllByFirstNameIgnoreCase(String fn);
+
+    // select * from author where first_name  like '%ku%'
+    List<Author> findAllByFirstNameContainingIgnoreCase(String fn);
+
+    // select * from author where first_name  like '%ku'
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String fn);
+
+    // select * from author where first_name  like 'ku%'
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String fn);
+
+    Optional<Author> findByEmail(String email);
+
+    int countAllByAge(int age);
+
+    void deleteAllByAge(int age);
 }
